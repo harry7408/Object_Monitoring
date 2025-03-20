ec491adf5d7ed7b045e1c4348824c906582328a5
harry <harry7408@naver.com>
2025-03-20 23:03:04 +0900

    [SpringBootApplication] chore : yaml 파일 설정
    Multipart 크기 제한, Swagger 설정

--- SpringBootApplication/ObjectMonitoring/src/main/java/com/harry/backend/ObjectMonitoringApplication.java
+++ SpringBootApplication/ObjectMonitoring/src/main/java/com/harry/backend/ObjectMonitoringApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObjectMonitoringApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ObjectMonitoringApplication.class,args);
+        SpringApplication.run(ObjectMonitoringApplication.class, args);
     }
 }
