4eeee707bfd1dfc723d532cd9cbadffbaaa47d0f
harry <harry7408@naver.com>
2025-06-28 22:21:48 +0900

    orbit 라이브러리 의존성 추가
    MVI 패턴 적용

--- AndroidClient/presentation/src/main/java/com/harry/presentation/main/CameraViewModel.kt
+++ AndroidClient/presentation/src/main/java/com/harry/presentation/main/CameraViewModel.kt
@@ -2,22 +2,45 @@ package com.harry.presentation.main
 
 import androidx.camera.core.ImageProxy
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.harry.domain.model.ApiResult
 import com.harry.domain.usecase.ObjectDetectUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.CoroutineExceptionHandler
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraViewModel @Inject constructor(
     private val objectDetectUseCase: ObjectDetectUseCase
-) : ViewModel() {
+) : ViewModel(), ContainerHost<MyScreenState, MyScreenSideEffect> {
 
-    fun onImageCaptured(image: ImageProxy) {
-        viewModelScope.launch {
-
-
+    override val container: Container<MyScreenState, MyScreenSideEffect> = container(
+        initialState = MyScreenState(),
+        buildSettings = {
+            this.exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+                intent {
+                    postSideEffect(
+                        MyScreenSideEffect.Toast(throwable.message.orEmpty())
+                    )
+                }
+            }
         }
-    }
+    )
 
+
+
+    fun onImageCaptured(imageProxy: ImageProxy) {
+
+    }
+}
+
+data class MyScreenState(
+    val apiResult: ApiResult? = null,
+    val errorMessage: String? = null,
+)
+
+sealed interface MyScreenSideEffect {
+    class Toast(val message: String) : MyScreenSideEffect
 }
